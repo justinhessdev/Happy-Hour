5e339d36c0a3dda433f1af6e4e81fff6ecf5c8ce
Nadia Kim <kim.nadia28@gmail.com>
2017-01-19 09:48:21 -0800

    started on passport, nav bar

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const
   dotenv = require('dotenv').load({silent: true}),
   express = require('express'),
   mongoose = require('mongoose'),
+  flash = require('connect-flash'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  MongoDBStore = require('connect-mongodb-session')(session),
+  passport = require('passport')
   request = require('request'),
   ejs = require('ejs'),
   ejsLayouts = require('express-ejs-layouts'),
@@ -13,7 +19,39 @@ const
   port  = (process.env.PORT || 3000),
 
 
+// //   // environment port
+// // const
+// //   // port = process.env.PORT || 3000,
+  mongoConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/passport-authentication'
+
+    // mongoose connection
+    mongoose.connect(mongoConnectionString, (err) => {
+      console.log(err || "Connected to MongoDB (passport-authentication)")
+    })
+
+    // will store session information as a 'sessions' collection in Mongo
+const store = new MongoDBStore({
+  uri: mongoConnectionString,
+  collection: 'sessions'
+    });
+
+  //middleware
   app = express()
+  app.use(express.static(__dirname + '/public'))
+  app.use(logger('dev'))
+  app.use(cookieParser())
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.json())
+  app.use(flash())
+  app.use(session({
+	   secret: 'boooooooooom',
+	   cookie: {maxAge: 60000000},
+	   resave: true,
+	   saveUninitialized: false,
+	   store: store
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
   /* Function for yelp call
    * ------------------------
    * set_parameters: object with params to search
@@ -69,22 +107,44 @@ const
     });
   };
 
-  app.get('/restaurants/:location' , (req, res) => {
-    request_yelp({term:'food', location:req.params.location}, function(error, response, body){
-      //console.log(error, response, body);
-      res.json(JSON.parse(body))
-    })
+  app.get('/search' , (req, res) => {
+    if (req.query.location){
+    request_yelp({term:'happy+hour', location:req.query.location}, function(error, response, body){
+      console.log(body);
+      var businesses = JSON.parse(body).businesses
+      var businessesJustTheGoodStuff = businesses.map((b) => {
+        return {name: b.name, id: b.id, rating: b.rating, location: b.location}
+      })
+      // res.json(businesses)
+      res.render('pages/search', {businesses: businessesJustTheGoodStuff})
+      // res.json(businessesJustTheGoodStuff)
+    })} else {
+      res.redirect('/')
+    }
   })
 
+
+  // // currentUser:
+  app.use((req, res, next) => {
+	app.locals.currentUser = req.user
+	app.locals.loggedIn = !!req.user
+
+	next()
+  })
+  //
   // ejs configuration
   app.set('view engine', 'ejs')
-    // app.use(ejsLayouts)
+  app.use(ejsLayouts)
 
   //root route
   app.get('/', (req, res) => {
   	res.render('pages/home')
   })
 
+  // app.get('/search', (req, res) => {
+  //   res.render('pages/search')
+  // })
+
   app.listen(port, (err) => {
-    console.log (err || "Server Runnign On Port " + port)
+    console.log (err || "Server Running On Port " + port)
   })
