7dca6f693ff30a616c32c7653ab53c36eacf0a75
justinhessdev <justinhessdev@gmail.com>
2017-01-20 11:39:19 -0800

    user added to posts

--- models/Post.js
+++ models/Post.js
@@ -7,7 +7,15 @@ const
     times: {type: String},
     description: {type: String},
     specials: {type: String},
-    neighborhood: {type: String}
+    neighborhood: {type: String},
+    _author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
   }, {timestamps: true})
+
+  postSchema.pre('findOne', function() {
+    this.populate('_author')
+  })
+// middleware that runs before it returns results back to you
+// when we do pre frind one we do someting before it spites back results to you
+// you have reference model without it popolutaed it autom
 
 module.exports = mongoose.model('Post', postSchema)
